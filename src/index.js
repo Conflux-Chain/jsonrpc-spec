3be2e23a15706ebe44199638ff9e86ec096563ec
PanaW <pana.wang@outlook.com>
2022-03-24 14:41:16 +0800

    init pub folder

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const CfxOpenRPC = require('./cfx/cfx.json');
+const CfxExamples = require('./cfx/examples.json');
+
+module.exports = {
+  CFX: {
+    OpenRPC: CfxOpenRPC,
+    Examples: CfxExamples,
+  },
+}
